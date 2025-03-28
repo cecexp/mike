c098aee7f329dfb1fc94c3871f754c8df8fe473a
cecexp <trabajos.cece@gmail.com>
2025-03-27 19:57:16 -0600

    Cambios en diseño
    Se realizó una mejora general en el diseño del código para ofrecer una apariencia más profesional y atractiva, optimizando la estructura y la experiencia del usuario.

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
     form.addEventListener("submit", function(event) {
         const username = document.getElementById("username").value.trim();
         const password = document.getElementById("password").value;
-        
+
         if (username.length < 3) {
             alert("El nombre debe tener al menos 3 caracteres.");
             event.preventDefault();
             return;
         }
-        
+
         if (password.length < 8) {
             alert("La contraseña debe tener al menos 8 caracteres.");
             event.preventDefault();
